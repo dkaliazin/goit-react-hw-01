0bb7696eb38090dcd1f6b87a19919a5343f356b9
dkaliazin <dkaliazin@gmail.com>
2024-06-16 21:57:52 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,49 @@ import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Product from './Product';
+import BookList from './Books';
+import { Alert } from "./Alert";
 
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
+const alertStyles = {
+  margin: 8,
+  padding: "12px 16px",
+  borderRadius: 4,
+  backgroundColor: "gray",
+  color: "white",
+};
+
+/*export default function App() {
+  return (
+    <>
+      <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+      <Product name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+        />
+    </> 
+  );
+}*/
 export default function App() {
   return (
-    <div>
-      <h1>PRODUCTAS</h1>
-
-      <Product />
-      <Product />
-      <Product />
-    </div>
+     <>
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
+    </>
   );
-}
+};
---
+++ src/components/Books.jsx
@@ -0,0 +1,8 @@
+const BookList = ({ books }) => (
+  <ul>
+	{books.map((book, index) => {
+		return <li key={index}>{book.name}</li>;
+	})}
+  </ul>
+);
+export default BookList;
