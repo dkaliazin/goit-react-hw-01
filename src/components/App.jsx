85a9379f988737aabf84a2a6b3f7aaeb365f94ff
dkaliazin <dkaliazin@gmail.com>
2024-06-16 22:54:58 +0300

    base task 1 without css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,50 +2,18 @@ import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Product from './Product';
-import BookList from './Books';
-import { Alert } from "./Alert";
-
-const favouriteBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" }
-];
-const alertStyles = {
-  margin: 8,
-  padding: "12px 16px",
-  borderRadius: 4,
-  backgroundColor: "gray",
-  color: "white",
-};
-
-/*export default function App() {
-  return (
-    <>
-      <h1>Books of the week</h1>
-      <BookList books={favouriteBooks} />
-      <Product name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-        />
-    </> 
-  );
-}*/
+import userData from "./UserData.json";
+import Profile from "./Profile.jsx"
 export default function App() {
   return (
-     <>
-      <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error">
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success">
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning">
-        Please update your profile contact information
-      </Alert>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </>
   );
 };
